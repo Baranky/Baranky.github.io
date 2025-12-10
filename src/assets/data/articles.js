2aa579516e08913b8b0010f5278088056631bbec
baranky <baranky2104@gmail.com>
2025-12-10 17:12:18 +0300

    update index

--- src/assets/data/articles.js
+++ src/assets/data/articles.js
@@ -7,7 +7,7 @@ export const articles = [
   {
     title: "Spring Boot Proje Yapısı ve Katmanlı Mimari Mantığı",
     link: "https://medium.com/@baranky2104/spring-boot-ile-i%CC%87lk-rest-apinizi-olu%C5%9Fturun-proje-yap%C4%B1s%C4%B1-ve-katmanl%C4%B1-mimari-fcda520a1278?postPublishedType=initial" ,  
-    date: "2024-11-20",
+    date: "2025-11-20",
   },
  
   // Daha fazla yazı ekleyebilirsiniz
